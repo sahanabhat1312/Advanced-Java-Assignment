b7eb70738bd505d45c92101b2204741a2c55c0d2
sahanabhat1312 <sahana64113@gmail.com>
2026-05-16 10:59:33 +0530

    Update comment to reflect new task description

--- program1b/p3.java
+++ program1b/p3.java
@@ -1,9 +1,5 @@
 //1b.
-//c.Write a java program for getting different colors through ArrayList interface and sort them using Collections.sort( ArrayListObj)
-
-
-
-
+//3.Write a java program for getting different colors through ArrayList interface and sort them using Collections.sort( ArrayListObj)
 package program1b;
 
 import java.util.*;
