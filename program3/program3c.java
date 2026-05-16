a27bce0486307f4addb9d1bebacbb80eced5443a
sahanabhat1312 <sahana64113@gmail.com>
2026-05-16 11:49:59 +0530

    Update program3c.java

--- program3/program3c.java
+++ program3/program3c.java
@@ -1,3 +1,11 @@
+//3c. String Exercise progams
+//Q6. Write a Java Program for Capitalizing the first letter of each word. using user defined function capitalizeWords()
+//Q7. Write a Java Program for Shortening a string to a specified length and adds an ellipsis using user defined function truncate()
+//Q8. Write a Java Program for Verifying if a string contains only numeric characters using user defined function isNumeric()
+//Q9. Write a Java Program for Creating a random string of a specified length using user defined function generateRandomString()
+//Q10. Write a Java Program for Counting the number of words in a string using user defined function countWords()
+
+
 package program3;
 
 public class program3c { 
