e3940574e68182659c7f81e3fbbba2d170c3e9ae
sahanabhat1312 <sahana64113@gmail.com>
2026-05-16 12:06:35 +0530

    Update ResultServlet.java

--- src/main/java/com/HTTP_REQ_RES/ResultServlet.java
+++ src/main/java/com/HTTP_REQ_RES/ResultServlet.java
@@ -1,3 +1,8 @@
+//10.b Build an Application to get Rollno, Studentname, Sub1, Sub2, Sub3, Sub4 and Sub5  through JSP called index.jsp with client sided validation and
+// submit to the servlet called ResultServlet and process all the fields with server sided validation and display all the data along with result 
+//( Pass if all subjects greater than 40%) and Average marks through result.jsp with a link to move to the client side
+
+
 package com.HTTP_REQ_RES;
 
 import java.io.IOException;
