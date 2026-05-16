0ec5016dec2982b6ab87f5b4a1aa87f2a980ea2f
sahanabhat1312 <sahana64113@gmail.com>
2026-05-16 11:48:36 +0530

    Update program3b.java

--- program3/program3b.java
+++ program3/program3b.java
@@ -1,3 +1,11 @@
+//3b. String Exercise progams
+//Q1. Write a Java Program for Checking if a given string is null or contains only whitespace using user defined function isNullOrEmpty().
+//Q2. Write a Java Program for Counting how many times a substring appears in a main string using user defined function countOccurrences()
+//Q3. Write a Java Program for Reversing the characters in a string using user defined function reverseString().
+//Q4. Write a Java Program for Checking if a string reads the same backward as forward (ignoring case and punctuation) using user defined function isPalindrome():
+//Q5. Write a Java Program for Eliminating all whitespace characters from a string using user defined function removeWhitespace()
+
+
 package program3;
 
 public class program3b {
