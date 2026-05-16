566866b7a42316b7ced6510afc4b752f52b44383
sahanabhat1312 <sahana64113@gmail.com>
2026-05-16 11:50:56 +0530

    Update ButtonExample.java

--- program4/ButtonExample.java
+++ program4/ButtonExample.java
@@ -1,3 +1,6 @@
+//4a. Basic hello program of Swing displaying the message Hello! VI C , Welcome to Swing Programming in Blue color plain font with font size of 32 using Jframe and Jlabel
+
+
 package program4;
 
 import java.awt.Font;
