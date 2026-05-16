88f901c02f22c44306f456b84350915d10ff69de
sahanabhat1312 <sahana64113@gmail.com>
2026-05-16 10:57:48 +0530

    Update p2.java

--- program1b/p2.java
+++ program1b/p2.java
@@ -1,3 +1,8 @@
+//1b
+//2.Write a java program for getting different colors through ArrayList interface and remove the 2nd element and color "Blue" from the ArrayList
+
+
+
 package program1b;
 
 import java.util.*;
