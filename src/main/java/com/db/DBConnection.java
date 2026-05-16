8747035917a98f58ad333981b7ef902346483ee0
sahanabhat1312 <sahana64113@gmail.com>
2026-05-16 12:09:08 +0530

    Update DBConnection.java

--- src/main/java/com/db/DBConnection.java
+++ src/main/java/com/db/DBConnection.java
@@ -1,3 +1,39 @@
+/* Develop a JDBC project using JDBC to append  the fields empno,  empname and basicsalary into the table Emp of the database Employee by getting the fields  through JSP and Generate the report as follows
+
+CREATE TABLE Emp (
+  Emp_NO INTEGER PRIMARY KEY,
+  Emp_Name TEXT NOT NULL,
+  Basicsalary  INTEGER 
+);
+
+
+-- insert
+INSERT INTO EMPLOYEE VALUES (101, 'Ramesh', 25000);
+INSERT INTO EMPLOYEE VALUES (102, 'Ravi', 20000);
+…..
+Add 5 records into Emp table of Employee database through Java with JDBC 
+
+and generate a salary report as follows
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~````
+Salary Report
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Emp_No     : 101
+Emp_Name: Ramesh'
+Basic           : 25000
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Emp_No     : 102
+Emp_Name: Ravi
+Basic           : 20000
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+….
+…
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~````
+Grand Salary   : 45000
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+*/
 package com.db;
 
 import java.sql.*;
